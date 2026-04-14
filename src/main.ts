1b9be41132159f8d6672dc458a817771454bcfd2
aliatwood <aliatwood0104@gmail.com>
2026-04-14 18:48:54 +0200

    uppdatera main.ts
    - använde dom-manipulation
    - skapade ett todo interface
    - skapade en todolist class med en array av todo-objekt, metod för att lägga till nya todos, metod för att markera todos som klara, metod för att hämta samtliga todos som e klara

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
 import './styles/styles.scss';
+
+const formEl = document.querySelector(".todo-form") as HTMLFormElement;
+const inputEl = document.getElementById("todo-input") as HTMLInputElement;
+const selectEl = document.getElementById("prioritet") as HTMLSelectElement;
+const container = document.getElementById("container") as HTMLInputElement;
+
+interface Todo{
+    task: string;
+    completed: boolean;
+    priority: number;
+    date: string;
+}
+
+class TodoList{
+    todos: Todo[] = [];
+
+    addTodo(task: string, priority: number): boolean {
+        if (!task.trim() || !priority) {
+            return false;
+        } else {
+            const date = new Date().toLocaleDateString('sv-SE');
+            this.todos.push({task, completed: false, priority, date});
+            return true;  
+        }
+    }
+
+    markTodoCompleted(todoIndex: number): void{
+        this.todos[todoIndex].completed = true;
+    }
+
+    getTodos(): Todo[] {
+        return this.todos;
+    }
+}
