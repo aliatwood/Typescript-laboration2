b8f67592397299e2b3dff13d873f13eac9e0d9e9
aliatwood <aliatwood0104@gmail.com>
2026-04-14 20:23:37 +0200

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: "/typescript-laboration2",
-    build: {
-        outDir: "docs"
-    }
-});
+  base: './',
+  build: {
+    outDir: 'docs'
+  }
+})
