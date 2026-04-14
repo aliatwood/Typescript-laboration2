2da3686eb40a261a7afe2cc80aa1a9e3795467c1
aliatwood <aliatwood0104@gmail.com>
2026-04-14 19:33:07 +0200

    Uppdaera index.html och main.ts
    La till en paragraf i html där felmeddelande hamnar.
    
    Skapade en instans av todolist, la till en addeventlistener på formuläret med validering, byggde min renderfunktion för att visa todos i dom och la till markera som klar-knapp med localstorage

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ const formEl = document.querySelector(".todo-form") as HTMLFormElement;
 const inputEl = document.getElementById("todo-input") as HTMLInputElement;
 const selectEl = document.getElementById("prioritet") as HTMLSelectElement;
 const container = document.getElementById("container") as HTMLInputElement;
+const felmeddelandeEl = document.getElementById("felmeddelande") as HTMLParagraphElement;
 
 interface Todo{
     task: string;
@@ -49,3 +50,50 @@ class TodoList{
     this.loadFromLocalStorage();
   }
 }
+
+const todoList = new TodoList();
+
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let task = inputEl.value;
+    let priority = parseInt(selectEl.value);
+
+    if (todoList.addTodo(task, priority)) {
+        felmeddelandeEl.style.display = "none";
+        inputEl.value = "";
+        renderTodos();
+    } else {
+        felmeddelandeEl.style.display = "block";
+    }
+})
+
+function renderTodos(): void{
+    container.innerHTML = "";
+
+    todoList.getTodos().forEach((todo, index) => {
+        let div = document.createElement("div");
+
+        div.classList.add("todos");
+
+        div.innerHTML = `
+            <h3>${todo.task}</h3>
+            <p>${todo.priority}</p>
+            <p>${todo.date}</p>
+            <p>${todo.completed ? "Klar ✅" : "Ej Klar ❌"}</p>
+            <button>Markera som klar</button>
+        `;
+
+        const btn = div.querySelector("button") as HTMLButtonElement;
+        btn.addEventListener("click", () => {
+            todoList.markTodoCompleted(index);
+            todoList.saveToLocalStorage();
+            renderTodos();
+        });
+
+
+        container.appendChild(div);
+    });
+}
+
+renderTodos();
